859b2bc0c1315ce64b8c711a252903e696406eac
Anton Spilnyy <anton.spilnyy@jetbrains.com>
2018-05-08 10:42:39 +0200

    reformat

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -4,27 +4,27 @@ using NUnit.Framework;
 
 namespace Tests
 {
-    [TestFixture]
-    public class Tests
+  [TestFixture]
+  public class Tests
+  {
+    private DummyClass d = new DummyClass();
+
+    [Test]
+    public void ShouldReturnZeroByDefault()
     {
-        private DummyClass d = new DummyClass();
-        
-        [Test]
-        public void ShouldReturnZeroByDefault()
-        {
-            Assert.AreEqual(0, d.ReturnPassedValue());
-        }
-        
-        [Test]
-        public void ShouldReturnPassedValue()
-        {
-            Assert.AreEqual(42, d.ReturnPassedValue(42));
-        }
-        
-        [Test]
-        public void NewTest()
-        {
-            Assert.AreEqual(43, d.ReturnPassedValue(43));
-        }
+      Assert.AreEqual(0, d.ReturnPassedValue());
     }
+
+    [Test]
+    public void ShouldReturnPassedValue()
+    {
+      Assert.AreEqual(42, d.ReturnPassedValue(42));
+    }
+
+    [Test]
+    public void NewTest()
+    {
+      Assert.AreEqual(43, d.ReturnPassedValue(43));
+    }
+  }
 }
